d2a7b719e3656386b40f8d94988c4add58ebbef9
Paula <pauayro@correo.ugr.es>
2018-02-05 17:01:28 +0100

    Añadido comando de preuba python

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import numpy as np
+# empty list
+squares = []
+
+# lists might contain items of different types
+squares = ['cat', 4, 3.2]
+
+# negative indices mean count backwards from end of sequence
+print squares[-1]
